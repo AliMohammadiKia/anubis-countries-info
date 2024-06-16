f7557a084274161d653f5314dc0016943ee1a60a
Maedeh Sarbanian <maedeh.sarbanian@gmail.com>
2024-06-16 18:56:08 +0330

    Edite JS header-dark & light icon-02

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,19 @@
  * Dark mode
  * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  */
+const darkIcon=document.querySelector("#dark-icon")
+const lightIcon=document.querySelector("#light-icon")
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.documentElement.setAttribute("data-theme","dark");
+     darkIcon.style.display="block"
+        lightIcon.style.display="none"
    
 }
 else{
     document.documentElement.setAttribute("data-theme","light");
+    darkIcon.style.display="none"
+        lightIcon.style.display="block"
 }
 
 
@@ -16,10 +22,15 @@ function changeMode(event) {
     const theme = document.documentElement.getAttribute("data-theme");
     if (theme==="light") {
         document.documentElement.setAttribute("data-theme","dark");
+        darkIcon.style.display="block"
+        lightIcon.style.display="none"
+
        
     }
     else if(theme==="dark"){
         document.documentElement.setAttribute("data-theme","light");
+        darkIcon.style.display="none"
+        lightIcon.style.display="block"
     }
 }
 /**!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
