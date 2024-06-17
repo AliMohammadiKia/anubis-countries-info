0ef20b9717e79d7d8128b663dedac972ed545c21
Ali Mohammadi Kia <alimohammadikia01@gmail.com>
2024-06-17 08:04:28 +0330

    merge/01-create-header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,28 @@
-/**!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- * Dark mode
- * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- */
-const darkIcon=document.querySelector("#dark-icon")
-const lightIcon=document.querySelector("#light-icon")
+// variables
+const darkIcon = document.querySelector("#dark-icon");
+const lightIcon = document.querySelector("#light-icon");
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.setAttribute("data-theme","dark");
-     darkIcon.style.display="block"
-        lightIcon.style.display="none"
-   
-}
-else{
-    document.documentElement.setAttribute("data-theme","light");
-    darkIcon.style.display="none"
-        lightIcon.style.display="block"
+// set default dark | light mode of user OS
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.documentElement.setAttribute("data-theme", "dark");
+  darkIcon.style.display = "block";
+  lightIcon.style.display = "none";
+} else {
+  document.documentElement.setAttribute("data-theme", "light");
+  darkIcon.style.display = "none";
+  lightIcon.style.display = "block";
 }
 
-
+// handle change dark | light mode
 function changeMode(event) {
-    const theme = document.documentElement.getAttribute("data-theme");
-    if (theme==="light") {
-        document.documentElement.setAttribute("data-theme","dark");
-        darkIcon.style.display="block"
-        lightIcon.style.display="none"
-
-       
-    }
-    else if(theme==="dark"){
-        document.documentElement.setAttribute("data-theme","light");
-        darkIcon.style.display="none"
-        lightIcon.style.display="block"
-    }
+  const theme = document.documentElement.getAttribute("data-theme");
+  if (theme === "light") {
+    document.documentElement.setAttribute("data-theme", "dark");
+    darkIcon.style.display = "block";
+    lightIcon.style.display = "none";
+  } else if (theme === "dark") {
+    document.documentElement.setAttribute("data-theme", "light");
+    darkIcon.style.display = "none";
+    lightIcon.style.display = "block";
+  }
 }
-/**!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- * Dark mode
- * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- */
