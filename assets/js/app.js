0efefcbd3748d353d9fcd288534bf75198dcc3b3
Maedeh Sarbanian <maedeh.sarbanian@gmail.com>
2024-06-22 15:23:22 +0330

    Add Local storage for dark | light mode with JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,14 @@
 const darkIcon = document.querySelector("#dark-icon");
 const lightIcon = document.querySelector("#light-icon");
 
-// set default dark | light mode of user OS
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+const isDefaultDarkMode = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
+const getTheme =
+  localStorage.getItem("theme") ?? (isDefaultDarkMode ? "dark" : "light");
+
+  // set default dark | light mode of user OS
+if (getTheme === "dark") {
   document.documentElement.setAttribute("data-theme", "dark");
   darkIcon.style.display = "block";
   lightIcon.style.display = "none";
@@ -17,12 +23,15 @@ if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
 function changeMode(event) {
   const theme = document.documentElement.getAttribute("data-theme");
   if (theme === "light") {
+    localStorage.setItem("theme", "dark");
     document.documentElement.setAttribute("data-theme", "dark");
     darkIcon.style.display = "block";
     lightIcon.style.display = "none";
   } else if (theme === "dark") {
+    localStorage.setItem("theme", "light");
     document.documentElement.setAttribute("data-theme", "light");
     darkIcon.style.display = "none";
     lightIcon.style.display = "block";
   }
 }
+
